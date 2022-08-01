edf507c04d169a10911f386b13067d5cf4aa75ca
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-08-02 00:44:12 +0100

    deploy

--- src/pages/signUpPage.js
+++ src/pages/signUpPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import {  useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Form from "components/Form";
 import Input from "components/Input";
